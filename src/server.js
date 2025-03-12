976f87dee2d9d965b7bfe55c4c87829b2f0f927e
Joshua Loller <joshua.loller72@gmail.com>
2025-03-12 14:26:39 -0400

    fixed plz

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3001; // Use Render's assigned port or default to 3001
+const PORT = process.env.PORT || 3001; 
 
 app.get("/", (req, res) => {
   res.send("Hello from Render!");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "localhost", () => {
   console.log(`Server running on port ${PORT}`);
 });
